31189b521953df392c66af6f7ccf1814cd92fc4f
Larry Weya <larryweya@gmail.com>
2013-01-03 15:59:39 +0300

    Pulled feed from local json files to emulate bamboo datasets

--- js/app.js
+++ js/app.js
@@ -2,19 +2,39 @@ angular.module('bamboo', []).
     config(function($routeProvider) {
         $routeProvider.
             when('/', {controller:AppController, templateUrl:'table.html'}).
-            when('/aggregations/:name', {controller:AggregationsController, templateUrl:'table.html'}).
+            when('/:id', {controller:AppController, templateUrl:'table.html'}).
             otherwise({redirectTo:'/'});
     });
 
-function AppController($scope, $http)
+function AppController($scope, $routeParams, $http)
 {
-    var promise = $http.get("table.html");
+    var url = "json/eaa43ef6baa54af4948303fd093d9756.json";
+    // check if an id was passed in
+    $scope.has_aggregation = false;
+    if($routeParams.id)
+    {
+        $scope.has_aggregation = true;
+        url = "json/" + $routeParams.id + ".json";
+    }
+    var promise = $http.get(url);
     promise.success(function(data){
-        $scope.columns = [{name: "name"}, {name: "age"}];
-        $scope.records = [{name: "Abe", age: 65}, {age: 78, name: "Peter"}];
+        $scope.columns = [];
+        // @todo: hack - columns should come from dataset info
+        angular.forEach(data[0], function(val, key){
+            $scope.columns.push(key);
+        })
+        $scope.records = data;
     });
 }
 
-function AggregationsController($scope, Project) {
-
+function AggregationsController($scope, $http) {
+    var promise = $http.get("json/aggregations.json");
+    promise.success(function(data){
+        $scope.aggregations = [];
+        angular.forEach(data, function(val, key){
+            key = key==""?"un-grouped":key;
+            var aggreagte = {name: key, id: val};
+            $scope.aggregations.push(aggreagte);
+        });
+    });
 }
